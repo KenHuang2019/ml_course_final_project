608d33992df73c3bb9e87c3e6bcaa94a39769d97
KenHuang2019 <kenhuang2019iii@gmail.com>
2020-12-24 03:37:56 +0000

    update attention

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -3,6 +3,10 @@ Sentiment analysis
 Ref:
 https://github.com/GoatWang/IIIMaterial/blob/master/08_InformationRetreival/main08.ipynb
 https://www.kaggle.com/anirudha16101/sentiment-analysis
+
+Attention:
+https://keras.io/examples/nlp/text_classification_with_transformer/
+https://keras.io/api/optimizers/adam/
 """
 
 import config
@@ -32,6 +36,9 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
+from tensorflow import keras
+from tensorflow.keras import layers
+
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation
@@ -39,6 +46,9 @@ from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import regularizers
 
 import matplotlib.pyplot as plt
+
+# Attention
+from multi_head_self_attention import MultiHeadSelfAttention, TransformerBlock, TokenAndPositionEmbedding
 
 def load_data(p):
     """
@@ -55,11 +65,12 @@ def load_data(p):
     #     df = df.append(tmp_df,ignore_index=True)
     
     # 嘗試切割子資料集
-    df_yelp = pd.read_table( p + file_names[0] ,header=None,sep='\t', names=["text", "label"])
-    df_imdb = pd.read_table( p + file_names[1] ,header=None,sep='\t', names=["text", "label"])
+    # df_yelp = pd.read_table( p + file_names[0] ,header=None,sep='\t', names=["text", "label"])
+    # df_imdb = pd.read_table( p + file_names[1] ,header=None,sep='\t', names=["text", "label"])
     df_amazon = pd.read_table( p + file_names[2] ,header=None,sep='\t', names=["text", "label"])
 
-    df = pd.concat([df_amazon,df_yelp,df_imdb])
+    # df = pd.concat([df_amazon,df_yelp,df_imdb])
+    df = df_amazon
 
     return df
 
@@ -107,10 +118,10 @@ def method_1_ann(X_train, X_test, y_train, y_test):
     model.add(Dense(1))
 
     model.compile(optimizer='adam',loss="mse")
-    earlystopper = EarlyStopping(monitor='val_loss', patience=50, verbose=0)
+    earlystopper = EarlyStopping(monitor='val_loss', patience=6, verbose=0)
     checkpoint =ModelCheckpoint(str(datetime.now().strftime("%d-%m-%Y_%H-%M-%S"))+".hdf5",save_best_only=True)
     callback_list=[earlystopper,checkpoint]
-    model.fit(X_train_dense, y_train.values, epochs=20, batch_size=config.BATCH_SIZE,validation_split=config.VALIDATION_SIZE,callbacks=callback_list)
+    model.fit(X_train_dense, y_train.values, epochs=40, batch_size=config.BATCH_SIZE,validation_split=config.VALIDATION_SIZE,callbacks=callback_list)
 
     pred_train = model.predict(X_train_dense,batch_size=config.BATCH_SIZE)
     pred_test = model.predict(X_test_dtm,batch_size=config.BATCH_SIZE)
@@ -120,38 +131,42 @@ def method_1_ann(X_train, X_test, y_train, y_test):
     correct_prediction = np.equal(pred_test_int, y_test.values)
     print(np.mean(correct_prediction))
 
-
-def method_2_LSTM(df):
+def preprocessing(df, vocab_size, max_length):
     """
-    嘗試做 word to vector 之後使用 LSTM
+    docstring
     """
     text = df['text'].tolist()
     label = df['label'].tolist()
 
     # word to vector
-    vocab_size=1000
     print("vocab size is", vocab_size)
     tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(text,vocab_size,max_subword_length=5)
     
     for i,sent in enumerate(text):
         text[i] = tokenizer.encode(sent)
 
-    max_length = 50
-
     text_added = pad_sequences(text,maxlen=max_length,padding ='post',truncating='post')
 
     # 切分訓練集與測試集
     training_size=int(len(text)*0.8)
+
     train_seq=text_added[:training_size]
-    train_labels=label[:training_size]
-
     test_seq=text_added[training_size:]
-    test_labels=label[training_size:]
 
-    train_labels=np.array(train_labels)
-    test_labels=np.array(test_labels)
+    train_labels=np.array(label[:training_size])
+    test_labels=np.array(label[training_size:])
     print("Total no of Training Sequence are",len(train_seq))
     print("Total no of Test Sequence are",len(test_seq))
+
+    return train_seq, train_labels, test_seq, test_labels
+
+def method_2_LSTM(df):
+    """
+    嘗試做 word to vector 之後使用 LSTM
+    """
+    vocab_size = 1000
+    max_length = 50
+    train_seq, train_labels, test_seq, test_labels = preprocessing(df, vocab_size, max_length)
 
     # Create model
     embedding_dim = 16
@@ -161,17 +176,81 @@ def method_2_LSTM(df):
         tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(embedding_dim,return_sequences=True)),
         tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(embedding_dim)),
         tf.keras.layers.Dense(6,activation='relu'),
+        tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(1,activation='sigmoid')
     ])
+
+    my_callbacks = [
+    tf.keras.callbacks.EarlyStopping(patience=6),
+    tf.keras.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.h5',monitor='val_loss',mode='auto',save_best_only=True),
+    ]
 
     #fit a model
     model.compile(loss="binary_crossentropy",optimizer='adam',metrics=['accuracy'])
     model.summary()
 
-    history = model.fit(train_seq,train_labels,epochs=20,validation_data=(test_seq,test_labels))
+    history = model.fit(train_seq,train_labels,epochs=40,validation_data=(test_seq,test_labels),callbacks=my_callbacks)
 
     plot_graphs(history, "accuracy", 'LSTM')
     plot_graphs(history, "loss", 'LSTM')
+
+def method_3_Attention(df):
+    """
+    Attention
+    """
+    vocab_size = 1000
+    max_length = 40
+    x_train, y_train, x_val, y_val = preprocessing(df, vocab_size, max_length)
+
+    embed_dim = 40  # Embedding size for each token
+    num_heads = 4  # Number of attention heads
+    ff_dim = 40  # Hidden layer size in feed forward network inside transformer
+
+    inputs = layers.Input(shape=(max_length,))
+    embedding_layer = TokenAndPositionEmbedding(max_length, vocab_size, embed_dim)
+    x = embedding_layer(inputs)
+    transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
+    x = transformer_block(x)
+    x = layers.GlobalAveragePooling1D()(x)
+    x = layers.Dropout(0.2)(x)
+    x = layers.Dense(20, activation="relu")(x)
+    x = layers.Dropout(0.1)(x)
+    outputs = layers.Dense(2, activation="softmax")(x)
+
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    # lr_schedule = keras.optimizers.schedules.ExponentialDecay(
+    #     initial_learning_rate=1e-2,
+    #     decay_steps=2,
+    #     decay_rate=0.9)
+    # optimizer = keras.optimizers.SGD(learning_rate=lr_schedule)
+    
+
+    # opt = keras.optimizers.Adam(learning_rate=0.001)
+    opt = tf.keras.optimizers.Adam(
+        learning_rate=0.0001,
+        beta_1=0.9,
+        beta_2=0.999,
+        epsilon=1e-07,
+        amsgrad=True,
+        name="Adam"
+    )
+
+    model.compile(optimizer=opt, loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    # model.compile("adam", "binary_crossentropy", metrics=["accuracy"])
+
+    # my_callbacks = [
+    # tf.keras.callbacks.EarlyStopping(patience=6),
+    # tf.keras.callbacks.ModelCheckpoint(filepath='model.{epoch:02d}-{val_loss:.2f}.h5',monitor='val_loss',mode='auto',save_best_only=True),
+    # ]
+    
+    history = model.fit(
+        x_train, y_train, batch_size=32, epochs=50, validation_data=(x_val, y_val)#,callbacks=my_callbacks
+    )
+
+    plot_graphs(history, "accuracy", 'Attention')
+    plot_graphs(history, "loss", 'Attention')
+
 
 def main():
     """
@@ -183,7 +262,9 @@ def main():
 
     # method_1_ann(X_train, X_test, y_train, y_test)
 
-    method_2_LSTM(df.reset_index(drop=True))
+    # method_2_LSTM(df.reset_index(drop=True))
+
+    method_3_Attention(df.reset_index(drop=True))
     
 
 if __name__ == "__main__":
